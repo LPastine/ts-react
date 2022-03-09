418493459f56f04b7912ba36dd95255b3a56f394
LPastine <lucianopastine.tech@gmail.com>
2022-03-09 07:16:40 -0300

    Declaring React Function Components with TS

--- rts/src/props/Child.tsx
+++ rts/src/props/Child.tsx
@@ -1,3 +1,14 @@
-export const Child = () => {
-  return <div>Hi There!</div>;
+interface ChildProps {
+  color: string;
+}
+
+export const Child = ({ color }: ChildProps) => {
+  return <div>{color}</div>;
+};
+
+// We explicitly declare the React Function Component as a TS React.FC type so
+// TS can recognize the default properties of React Components
+// such as (propTypes, displayName, defaultProps, contextTypes)
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+  return <div>{color}</div>;
 };
